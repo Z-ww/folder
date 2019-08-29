4b156bdf5d54f3875463cc5b5257b00c11dc380d
gaoxiangyu <you@example.com>
2019-08-29 20:35:00 +0800

    g

---
+++ app1/src/views/food.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+       <headers></headers>
+    </div>
+</template>
+<script>
+import headers from '../components/HelloWorld'
+export default{
+    data(){
+        return {
+            da1: '',
+		    da2: '',
+        }
+    },
+    components:{headers},
+    created(){
+        this.gain()
+    },
+    methods:{
+        gain() {
+				// this.$http.get('https://elm.cangdu.org/v2/index_entry', ).then((data) => {
+                //     var dat = data.data;
+                //     console.log(data)
+				// 	this.da1 = dat.splice(0, 8);
+
+				// })
+				// this.$http.get('https://elm.cangdu.org/v2/index_entry', ).then((data) => {
+				// 	var dat = data.data;
+				// 	this.da2 = dat.splice(8, 16);
+
+				// })
+			}
+    }
+}
+</script>
+<style>
+
+</style>
