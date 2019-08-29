3eab1264e24dda70a5d3bb67bb387984babc4dc3
gaoxiangyu <you@example.com>
2019-08-29 19:42:04 +0800

    a

--- app1/src/App.vue
+++ app1/src/App.vue
@@ -17,34 +17,6 @@ export default {
 </script>
 
 <style>
-<<<<<<< HEAD
-
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    width:100%;
-    height:100%;
-  }
-  *{
-    margin:0;
-    padding:0;
-  }
-  html,body{
-    width:100%;
-    height:100%;
-  }
-  ul{list-style:none}
-  a{text-decoration: none;}
-  img{vertical-align:top;border:none}
-
-  .clearf:after,
-  .clearf:before {
-    content: "";
-    display: block;
-    clear: both;
-  }
-=======
 *{
   margin: 0;
   padding: 0;
@@ -56,5 +28,4 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
->>>>>>> 83ff969f2f8bc4fd3069cb5bfb66e9b7f849a045
 </style>
