1eacabfe774239886052b09ac918cbb7894a35f2
zhaolinfei <1103905129@qqcom>
2019-08-29 17:08:00 +0800

    赵林飞

--- app1/src/components/HelloWorld.vue
+++ app1/src/components/HelloWorld.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="hello">
-  	
+      <span>{{left}}</span>
+      <div>{{name}}</div>
+      <p>{{right}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
+  props:{
+      left:{
+          type:String
+      },
+      name:{
+          type:String
+      },
+      right:{
+          type: String
+      }
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+
     }
   }
 }
@@ -21,5 +34,29 @@ export default {
 	width:100%;
 	height:50px;
 	background-color:#0000FF;
+  overflow: hidden;
+  position: fixed;
+  top: 0;
+}
+.hello>span{
+  float: left;
+  padding-left: 10px;
+  font-size: 20px;
+  color: #eee;
+  line-height: 50px;
+}
+.hello>div{
+  float: left;
+  padding-left: 160px;
+  font-size: 24px;
+  color: #eee;
+  line-height: 50px;
+}
+.hello>p{
+  float: right;
+  font-size: 12px;
+  padding-right: 30px;
+  color: #eee;
+  line-height: 50px;
 }
 </style>
