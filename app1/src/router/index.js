561a4901d3dee159abf2c7eeb562371be2da6b3d
gaoxiangyu <you@example.com>
2019-08-30 09:15:47 +0800

    a

--- app1/src/router/index.js
+++ app1/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '../views/home'
-import food from '../views/food'
+import foodList from '../views/foodList'
 
 Vue.use(Router)
 
@@ -18,9 +18,9 @@ export default new Router({
       component: () => import(/* webpackChunkName: "about" */ '../views/mine.vue')
     },
     {
-      path: '/food',
-      name: 'food',
-      component:food
+      path: '/foodList',
+      name: 'foodList',
+      component:foodList
     }
   ]
 })
